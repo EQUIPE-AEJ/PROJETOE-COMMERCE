280687d1f8125ec8d451a8a301d9a82805cf4cd2
jaoviitor <joaoviiitorvieira@gmail.com>
2023-05-16 09:09:12 -0300

    add rota cliente

---
+++ routes/cliente.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('../mysql').pool;
+const bcrypt = require('bcrypt');
+
+
+//RETORNA TODOS OS CLIENTES
+router.get('/', (req, res, next) =>{
+    mysql.getConnection((error, conn) =>{
+        if(error){ return res.status(500).send({ error: error }) };
+        conn.query(
+            'SELECT * FROM Cliente;',
+            (error, resultado, fields) =>{
+                conn.release();
+                if(error){ return res.status(500).send({ error: error }) };
+                return res.status(200).send({response: resultado});
+            }
+        )
+    })
+});
+
+//RETORNA UM CLIENTE ESPECIFICO
+router.get('/:Id_Cliente', (req, res, next) =>{
+    mysql.getConnection((error, conn) =>{
+        if(error){ return res.status(500).send({ error: error }) };
+        conn.query(
+            'SELECT * FROM Cliente WHERE Id_Cliente = ?;',
+            [req.params.Id_Cliente],
+            (error, resultado, fields) =>{
+                conn.release();
+                if(error){ return res.status(500).send({ error: error }) };
+                return res.status(200).send({response: resultado});
+            }
+        )
+    })
+});
