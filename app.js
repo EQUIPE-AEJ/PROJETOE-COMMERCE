4d43e8c77f00c9bcb9f0f823f6bddfd77dfd3540
jaoviitor <joaoviiitorvieira@gmail.com>
2023-05-16 09:21:44 -0300

    configuração app

--- app.js
+++ app.js
@@ -4,5 +4,29 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const rotaCliente = require('./routes/cliente');
 
 app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/cliente', rotaCliente);
+
+//TRATAMENTO PARA QUANDO NÃO ENCONTRA UMA ROTA
+app.use((req, res, next) =>{
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
+
+module.exports = app;
