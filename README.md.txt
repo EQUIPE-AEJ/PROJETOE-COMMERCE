b6ecb6f3f8c50c1c6143754c533a8f9024bb05c9
Ana Beatriz <anaporfirioalbuquerque@gmail.com>
2023-05-30 12:31:40 -0300

    Atualizações no CSS e JS

---
+++ README.md.txt
@@ -0,0 +1,4 @@
+Atualizei o footer
+Cards de Benefícios
+Cadastro de e-mail
+Também adicionei uma página de js para algumas funcionalidades do site
---
+++ script.js
@@ -0,0 +1,19 @@
+const contactInput = document.getElementById('contactInput');
+  const emailCheckbox = document.getElementById('emailUpdates');
+  const smsCheckbox = document.getElementById('smsUpdates');
+
+  emailCheckbox.addEventListener('change', function() {
+    if (this.checked) {
+      contactInput.placeholder = 'E-mail';
+    } else {
+      contactInput.placeholder = 'Selecione uma opção';
+    }
+  });
+
+  smsCheckbox.addEventListener('change', function() {
+    if (this.checked) {
+      contactInput.placeholder = 'Número de telefone';
+    } else {
+      contactInput.placeholder = 'Selecione uma opção';
+    }
+  });
